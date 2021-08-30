55aaf4fbcf721c46080bf0d03e189bf61a2acfc9
Md Nazmul Hasan <nazmul.hasan@newtonx.com>
2021-08-31 03:15:08 +0600

    migration files added

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template
 from flask_restful import Api, reqparse
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import *
 
 app = Flask(__name__)
@@ -9,6 +10,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 
 db = SQLAlchemy(app)
+migrate = Migrate(app=app, db=db)
 
 # REST API setup
 api = Api(app)
