58ae1e092193b8854d47bbb2de4853fe274a7b8c
Md Nazmul Hasan <nazmul.hasan@newtonx.com>
2021-08-31 02:53:01 +0600

    setup SQLAlchemy done

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 import os
 from flask import Flask, render_template
 from flask_restful import Api, reqparse
-
+from flask_sqlalchemy import SQLAlchemy
+from config import *
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+
+db = SQLAlchemy(app)
 
 # REST API setup
 api = Api(app)
